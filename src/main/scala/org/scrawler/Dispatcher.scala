85682002a8f170914c4304b00aed0a29263c1c22
Mike Lewis <ft.mikelewis@gmail.com>
2012-01-24 00:57:37 -0800

    Fix indent

--- src/main/scala/org/scrawler/Dispatcher.scala
+++ src/main/scala/org/scrawler/Dispatcher.scala
@@ -2,5 +2,5 @@ package org.scrawler
 import akka.dispatch.Dispatchers
 
 object Dispatcher {
-	val dispatcher = Dispatchers.newExecutorBasedEventDrivenWorkStealingDispatcher("crawler-dispatcher").build
+  val dispatcher = Dispatchers.newExecutorBasedEventDrivenWorkStealingDispatcher("crawler-dispatcher").build
 }
