8f2b228143f9b099d9f186b4f0b1ae7c0f982edc
Mike Lewis <ft.mikelewis@gmail.com>
2012-02-05 00:06:43 -0800

    More format

--- src/main/scala/org/scrawler/ParsedDocument.scala
+++ src/main/scala/org/scrawler/ParsedDocument.scala
@@ -5,14 +5,14 @@ import com.ning.http.client.Response
 import scala.collection.JavaConversions._
 
 class ParsedDocument(val response: Response, val doc: Document) extends FinalDocument {
-  val urls = doc.select("a[href],frame[src],link[href],script[src]").foldLeft(List.empty[String]){(list, e) =>
+  val urls = doc.select("a[href],frame[src],link[href],script[src]").foldLeft(List.empty[String]) { (list, e) =>
     val url = e.tagName() match {
       case "a" | "link" => e.attr("abs:href")
       case "frame" | "script" => e.attr("abs:src")
     }
     url :: list
   }
-  
+
   val statusCode = response.getStatusCode
   val headers = GeneralUtils.getHeadersFromResponse(response)
   val body = response.getResponseBody
