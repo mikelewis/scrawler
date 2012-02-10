b9a0d6e3168c99a3082d68866446d9cdc7e0c4e8
Mike Lewis <ft.mikelewis@gmail.com>
2012-02-10 11:10:27 -0800

    Canonicalize URLS

--- src/main/scala/org/scrawler/UrlUtils.scala
+++ src/main/scala/org/scrawler/UrlUtils.scala
@@ -1,6 +1,10 @@
 package org.scrawler
 import com.ning.http.util.AsyncHttpProviderUtils
 import java.net.URI
+import java.net.URLDecoder
+import scala.collection.immutable.TreeMap
+import java.net.URLEncoder
+import java.io.UnsupportedEncodingException
 
 object UrlUtils {
   def isValidUrl(url: String): Boolean = {
@@ -20,8 +24,18 @@ object UrlUtils {
   // assumed to be a valid uri
   def sanitizeUrl(uri: URI) = {
     // strip fragments
-    new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(),
+    val newUri = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(),
       uri.getQuery(), null)
+
+    val port = newUri.getPort
+    // Option to safe guard against null (thanks Java :/ )
+    var queryString = generateCanonicalQueryString(Option(newUri.getQuery()))
+
+    newUri.getScheme().toLowerCase +
+      "://" + newUri.getHost.toLowerCase +
+      (if (port != -1 && port != 80) ":" + port else "") +
+      newUri.getPath +
+      (if (queryString == "") "" else "?" + queryString)
   }
 
   def createURI(url: String): Option[URI] = {
@@ -29,6 +43,43 @@ object UrlUtils {
       Some(AsyncHttpProviderUtils.createUri(url))
     } catch {
       case e => None
+    }
+  }
+
+  def generateCanonicalQueryString(path: Option[String]): String = {
+    path.map { p =>
+      val resultStringBuilder = p.split("&").foldLeft(TreeMap[String, String]()) { (map, pair) =>
+        // decode tokens
+        val tokens = pair.split("=", 2).map { v =>
+          try {
+            URLDecoder.decode(v, "UTF-8")
+          } catch {
+            case x: UnsupportedEncodingException => v
+          }
+        }
+
+        tokens.length match {
+          case 1 if pair(0) == '=' => map + ("" -> tokens(0))
+          case 1 => map + (tokens(0) -> "")
+          case 2 => map + (tokens(0) -> tokens(1))
+
+          // filter out useless params like session or utm_*
+        }
+      }.foldLeft(new StringBuilder()) {
+        case (queryString, (key, value)) =>
+          queryString.appendAll(percentEncodeRfc3986(key)).appendAll("=").appendAll(percentEncodeRfc3986(value)).appendAll("&")
+      }
+
+      (if (resultStringBuilder.endsWith("&")) resultStringBuilder.dropRight(1) else resultStringBuilder).mkString
+    }.getOrElse("")
+  }
+
+  // Thanks http://stackoverflow.com/questions/2993649/how-to-normalize-a-url-in-java
+  def percentEncodeRfc3986(str: String): String = {
+    try {
+      URLEncoder.encode(str, "UTF-8")
+    } catch {
+      case x: UnsupportedEncodingException => str
     }
   }
 }
--- src/test/scala/org/scrawler/UrlUtilsSpec.scala
+++ src/test/scala/org/scrawler/UrlUtilsSpec.scala
@@ -22,10 +22,93 @@ class UrlUtilsSpec extends MasterSuite {
     }
   }
 
+  describe("generateCanonicalQueryString") {
+    it("should return an empty string if passing in None") {
+      UrlUtils.generateCanonicalQueryString(None) should be("")
+    }
+
+    it("should return correct query string") {
+      UrlUtils.generateCanonicalQueryString(Some("flag=true&name=mike")) should be("flag=true&name=mike")
+    }
+
+    it("should return query string in lexical order") {
+      UrlUtils.generateCanonicalQueryString(Some("name=mike&bat=3&log=true")) should be("bat=3&log=true&name=mike")
+    }
+
+    it("should handle empty value in (key,value) pair") {
+      UrlUtils.generateCanonicalQueryString(Some("name=mike&bat=3&log=")) should be("bat=3&log=&name=mike")
+    }
+
+    it("should handle empty key in (key,value) pair") {
+      UrlUtils.generateCanonicalQueryString(Some("name=mike&=3&log=true")) should be("=3&log=true&name=mike")
+    }
+
+    it("should encode str") {
+      UrlUtils.generateCanonicalQueryString(Some("flag=true 3&name=mike@gmail")) should be("flag=true+3&name=mike%40gmail")
+    }
+  }
+
   describe("sanitizeUrl") {
+
+    it("should accept url with query string") {
+      val uri = new URI("http://google.com?name=3")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com?name=3")
+    }
+
+    it("should accept url with path(slash) and query string") {
+      val uri = new URI("http://google.com/index/car/index.php/?name=3")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com/index/car/index.php/?name=3")
+    }
+
+    it("should accept url with path( non file) and query string") {
+      val uri = new URI("http://google.com/index/car/index?name=3")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com/index/car/index?name=3")
+    }
+
+    it("should accept url with query string ( and slash )") {
+      val uri = new URI("http://google.com/?name=3")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com/?name=3")
+    }
+
+    it("should accept url with no query string") {
+      val uri = new URI("http://google.com/index.html")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com/index.html")
+    }
+
+    it("should accept https") {
+      val uri = new URI("https://google.com/index.html")
+      UrlUtils.sanitizeUrl(uri).toString() should be("https://google.com/index.html")
+    }
+
     it("should strip fragments") {
       val uri = new URI("http://google.com#hi")
       UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com")
+    }
+
+    it("should not strip out trailing slash") {
+      val uri = new URI("http://google.com/")
+      UrlUtils.sanitizeUrl(uri).toString() should not be ("http://google.com")
+    }
+
+    it("should strip out port 80") {
+      val uri = new URI("http://google.com:80")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com")
+    }
+
+    it("should strip out blank port") {
+      val uri = new URI("http://google.com:")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com")
+    }
+
+    it("should lowercase url") {
+      val uri = new URI("HTTP://GoogLe.com")
+      UrlUtils.sanitizeUrl(uri).toString() should be("http://google.com")
+    }
+
+    it("should keep unreserved chars") {
+      val uri = new URI("http://www.example.com/%7Eusername?name=mike%2Dlewis&birth=united%2Estates&phone=555%5F555")
+      UrlUtils.sanitizeUrl(uri)
+        .toString() should be("http://www.example.com/~username?birth=united.states&name=mike-lewis&phone=555_555")
     }
   }
 
